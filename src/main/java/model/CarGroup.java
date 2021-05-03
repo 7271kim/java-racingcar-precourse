9af7f74c415ebe5ef74a2def8847908279935a3d
김석진 <7271kim@naver.com>
2021-05-03 23:33:22 +0900

    feat : 기능 목록 4 -2 구현
    목록 4 :  CarGroup은 일급 콜렉션으로  List<Car>를 유일하게 인스턴스 변수로 가지며 아래와 같은 기능들을
    가진다.
    - 인스턴스 변수 크기와 동일한 배열에 1 - 9 사이 랜덤 값을 할 당한다.
    
    [변경점]
    - CarGroup.java : getRandomNumber(), getRandomNumbers() 신규 추가
    - CarGroupTest.java : 기존 createValidation() 2개로 분리 및
    getRandomNumberTest(), getRandomNumbersTest(), checkHasAllNumber(),
    checkOneToNine() 추가

--- src/main/java/model/CarGroup.java
+++ src/main/java/model/CarGroup.java
@@ -3,6 +3,8 @@ package model;
 import java.util.List;
 
 public class CarGroup {
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 9;
 
 	private List<Car> cars;
 
@@ -26,6 +28,19 @@ public class CarGroup {
 		if (cars.size() < 2) {
 			throw new IllegalArgumentException("최소 2개 이상의 배열이 들어와야 합니다. 생성자를 확인하세요");
 		}
+	}
+
+	protected int getRandomNumber() {
+		return (int)((Math.random() * (MAX_NUMBER - MIN_NUMBER + 1)) + MIN_NUMBER);
+	}
+
+	public int[] getRandomNumbers() {
+		int size = cars.size();
+		int[] result = new int[size];
+		for (int index = 0; index < size; index++) {
+			result[index] = getRandomNumber();
+		}
+		return result;
 	}
 
 }
--- src/test/java/model/CarGroupTest.java
+++ src/test/java/model/CarGroupTest.java
@@ -1,5 +1,6 @@
 package model;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarGroupTest {
+	private static final int ENOUGH_BIG_NUMBER = 10000;
 	private CarGroup carGroup;
 
 	@BeforeEach
@@ -23,17 +25,49 @@ public class CarGroupTest {
 
 	@Test
 	@DisplayName("carGroup의 멤버변수 List<Car> 최소 2개 이상이다.")
-	public void createValidation() {
+	public void checkValidation() {
 		assertThrows(IllegalArgumentException.class, () -> {
 			List<Car> cars = new ArrayList<>();
 			cars.add(new Car("김석진차"));
 			carGroup = new CarGroup(cars);
 		});
+	}
 
+	@Test
+	@DisplayName("carGroup의 멤버변수 List<Car>은 크기가 0이 될 수 없다")
+	public void checkValidationSize() {
 		assertThrows(IllegalArgumentException.class, () -> {
 			List<Car> cars = new ArrayList<>();
 			carGroup = new CarGroup(cars);
 		});
+	}
+
+	@Test
+	@DisplayName("1부터 9까지의 수를 random으로 뽑는지 테스트")
+	public void getRandomNumberTest() {
+		int[] cache = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		checkOneToNine(cache);
+		checkHasAllNumber(cache);
+	}
+
+	@Test
+	@DisplayName("List<Car> 크기의 1-9까지의 랜덤 수를 담은 메서트  getRandomNumbers 테스트")
+	public void getRandomNumbersTest() {
+		assertThat(carGroup.getRandomNumbers().length).isEqualTo(3);
+	}
+
+	private void checkHasAllNumber(int[] cache) {
+		for (int index = 1; index < 9; index++) {
+			assertThat(cache[index]).isEqualTo(1);
+		}
+	}
+
+	private void checkOneToNine(int[] cache) {
+		for (int index = 0; index < ENOUGH_BIG_NUMBER; index++) {
+			int randomNumber = carGroup.getRandomNumber();
+			assertThat(randomNumber).isBetween(1, 9);
+			cache[randomNumber] = 1;
+		}
 	}
 
 }
